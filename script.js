7ea55a896a1f730ce5a4d3c2cab1deeec08e1923
allazaoallado <allanv985@gmail.com>
2023-01-29 14:51:11 -0300

    habilitado os botões para tradução

--- script.js
+++ script.js
@@ -31,5 +31,72 @@ function saudacao(){
 }
 
 
+function greeting(){
+    data = new Date()
+    hora = data.getHours()
+
+    if(hora < 12 && hora >= 6){
+        return 'good morning'
+    }else if(hora < 18 && hora >= 12){
+        return 'good afternoon'
+    }else{
+        return 'good night'
+    }
+}
+
+function english(){
+    let about = window.document.getElementById('sobre')
+    let text_about = window.document.getElementById('texto-sobre')
+    let contacts = window.document.getElementById('contatos')
+    let projects = window.document.getElementById('h2-projetos')
+    let average_calculator = window.document.getElementById('calculadora-de-media')
+    let bhaskara_calculator = window.document.getElementById('calculadora-de-bhaskara')
+    let discount_calculator = window.document.getElementById('calculadora-desconto')
+    let footer = window.document.getElementById('footer')
+
+    text_about.innerHTML = `Hello, ${greeting()}! I'm Allan, I'm ${idade()} years old and I'm a technology and programming lover. I intend to become a great programmer, of course I'm just starting out, but for me not even the sky is the limit. In addition to programming, I also like to read and play video games, I love a good story and good content.`
+
+    about.innerHTML = `About:`
+
+    contacts.innerHTML = `Contacts:`
+
+    projects.innerHTML = `Projects:`
+
+    average_calculator.innerHTML ='Average Calculator'
+
+    bhaskara_calculator.innerHTML = 'Bhaskara Calculator'
+
+    discount_calculator.innerHTML = 'Discount Calculator'
+    
+    footer.innerHTML = 'Developed by Allan Souza Cordeiro'
+}
+
+function portuguese(){
+    let sobre = window.document.getElementById('sobre')
+    let texto_sobre = window.document.getElementById('texto-sobre')
+    let contatos = window.document.getElementById('contatos')
+    let projetos = window.document.getElementById('h2-projetos')
+    let calculadora_de_media = window.document.getElementById('calculadora-de-media')
+    let calculadora_de_bhaskara = window.document.getElementById('calculadora-de-bhaskara')
+    let calculadora_de_desconto = window.document.getElementById('calculadora-desconto')
+    let footer = window.document.getElementById('footer')
+
+    texto_sobre.innerHTML = `Olá, ${saudacao()}! eu sou o Allan, tenho ${idade()} anos e sou um amante da tecnologia e programação. Pretendo me tornar um grande programador, claro que estou só no começo, porém para mim nem o céu é o limite. Além de programar gosto também de ler e jogar, amo uma boa história e um bom conteudo.`
+
+    sobre.innerHTML = `Sobre:`
+
+    contatos.innerHTML = `Contatos:`
+
+    projetos.innerHTML = `Projetos:`
+
+    calculadora_de_media.innerHTML ='Calculadora de Média'
+
+    calculadora_de_bhaskara.innerHTML = 'Calculadora de Bhaskara'
+
+    calculadora_de_desconto.innerHTML = 'Calculadora de Desconto'
+    
+    footer.innerHTML = 'Desenvolvido por Allan Souza Cordeiro'
+}
+
 
 
